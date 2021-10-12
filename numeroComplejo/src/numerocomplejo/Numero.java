e0069567c4cec119e3003e6ef3a195870e496821
quot;AdrianCelayaRquot <quot;a1159367@uabc.edu.mxquot>
2021-10-12 01:37:32 -0700

    Nuevo avance

---
+++ numeroComplejo/src/numerocomplejo/Numero.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numerocomplejo;
+
+/**
+ *
+ * @author ramjam
+ */
+public class Numero {
+  private int valor;
+
+  public Numero() {
+  }
+
+  public Numero(int valor) {
+    this.valor = valor;
+  }
+  
+  public int getValor() {
+    return valor;
+  }
+
+  public void setValor(int valor) {
+    this.valor = valor;
+  }
+
+  
+   
+}
