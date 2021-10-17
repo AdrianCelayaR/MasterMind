1ad6b3bfc86ad2eaff447724bdfc08cabb36930c
quot;AdrianCelayaRquot <quot;a1159367@uabc.edu.mxquot>
2021-10-17 13:03:03 -0700

    Nuevo avance

--- numeroComplejo/src/numerocomplejo/NumeroComplejo.java
+++ numeroComplejo/src/numerocomplejo/NumeroComplejo.java
@@ -5,6 +5,8 @@
  */
 package numerocomplejo;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ramjam
@@ -15,19 +17,41 @@ public class NumeroComplejo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int opc;
+        Scanner scn = new Scanner(System.in);
         Complejo c1 = new Complejo(2,3);
         Complejo c2 = new Complejo(4,-7);
         Complejo prueba = new Complejo();
-        prueba = Complejo.sumar(c1, c2);
-        System.out.println(prueba);
-        prueba = c1.sumar(c2);
-        System.out.println(prueba);
-        prueba = c1.restar(c2);
-        System.out.println(prueba);
-        prueba = c1.producto(c2);
-        System.out.println(prueba);
-        prueba = c1.division(c2);
-        System.out.println(prueba);
+        do{
+            System.out.println("\t\tCALCULADORA DE NUMEROS COMPLEJOS");
+            System.out.println("[1] Suma\n[2] Restar\n[3] Multiplicar\n[4] Dividir\n[5] Salir\n");
+            System.out.print("Seleccione una opcion [ ]\b\b");
+            opc = scn.nextInt();
+            
+            switch(opc){
+                case 1:
+                    prueba = c1.sumar(c2);
+                    System.out.println(prueba);
+                    break;
+                case 2:
+                    prueba = c1.restar(c2);
+                    System.out.println(prueba);
+                    break;
+                case 3:
+                    prueba = c1.producto(c2);
+                    System.out.println(prueba);
+                    break;
+                case 4:
+                    prueba = c1.division(c2);
+                    System.out.println(prueba);
+                    break;
+                case 5:
+                    System.out.println("BYE BYE!!!");
+                    break;
+            }
+        }while(opc!=5);
+        //prueba = Complejo.sumar(c1, c2);
+        //System.out.println(prueba);
         
     }
     
